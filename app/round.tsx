00a15833bce95ad01936f6df0296439d18fd4cbc
BrenoFischer <breno_perricone@hotmail.com>
2026-02-10 16:28:44 -0300

    initial recording UI + functionalities

--- app/round.tsx
+++ app/round.tsx
@@ -1,5 +1,5 @@
 import { GameContext } from '@/context/GameContext';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -15,11 +15,20 @@ import Discussion from './discussion';
 import WithSidebar from '@/components/withSideBar';
 import { useTranslation } from '@/translations';
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import Dot from '@/components/dot';
+import {
+  useAudioRecorder,
+  AudioModule,
+  RecordingPresets,
+  setAudioModeAsync,
+  useAudioRecorderState,
+} from 'expo-audio';
 
 export default function RoundScreen() {
   const { game, nextRound, previousRound, getCurrentQuestion } =
     useContext(GameContext);
+  const [ isRecording, setIsRecording ] = useState(false);
   const { t } = useTranslation();
 
   const totalRounds = game.players.length * 2;
@@ -32,6 +41,49 @@ export default function RoundScreen() {
   const playerThatAsks = round.playerThatAsks;
   const playerThatAnswers = round.playerThatAnswers;
   const question = t(getCurrentQuestion(), { ns: 'categories' });
+
+  // Initialize the recorder with a preset
+  const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
+  
+  // This hook provides the real-time "timer" (currentTime)
+  // The '100' is the update interval in milliseconds
+  const recorderState = useAudioRecorderState(audioRecorder, 100);
+
+  useEffect(() => {
+    (async () => {
+      const status = await AudioModule.requestRecordingPermissionsAsync();
+      if (!status.granted) {
+        alert('Permission to access microphone was denied');
+      }
+
+      setAudioModeAsync({
+        playsInSilentMode: true,
+        allowsRecording: true,
+      });
+    })();
+  }, []);
+
+  const startRecording = async () => {
+    setIsRecording(true)
+    await audioRecorder.prepareToRecordAsync();
+    audioRecorder.record();
+  };
+
+  const stopRecording = async () => {
+    setIsRecording(false);
+    // The recording will be available on `audioRecorder.uri`.
+    await audioRecorder.stop();
+  }
+
+  const formatTime = (miliSeconds: number) => {
+    const totalSeconds = Math.floor(miliSeconds / 1000);
+    const mins = Math.floor(totalSeconds / 60);
+    const secs = totalSeconds % 60;
+    const formattedMins = mins.toString().padStart(2, '0');
+    const formattedSecs = secs.toString().padStart(2, '0');
+
+    return `${formattedMins}:${formattedSecs}`;
+  };
 
   const handleNextRound = () => {
     nextRound();
@@ -55,23 +107,21 @@ export default function RoundScreen() {
             style={{
               alignItems: 'center',
               flexDirection: 'row',
-              marginVertical: verticalScale(12),
+              marginVertical: verticalScale(30),
             }}
           >
             <Text style={styles.headerCategoryTitle}>
               {t('Round')} {game.currentRound} {t('of')} {totalRounds}
             </Text>
             <Dot color={colors.orange[200]} />
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Text style={styles.headerCategoryTitle}>
-                {t('Category')}
-              </Text>
-              <Dot color={colors.orange[200]} />
-              <Text style={styles.headerCategoryTitle}>
-                {t(game.category || '')}
-              </Text>
+            <Text style={styles.headerCategoryTitle}>
+              {t('Category')}
+            </Text>
+            <Dot color={colors.orange[200]} />
+            <Text style={styles.headerCategoryTitle}>
+              {t(game.category || '')}
+            </Text>
             </View>
-          </View>
         </View>
 
         <View>
@@ -84,6 +134,27 @@ export default function RoundScreen() {
             <View style={{ flexDirection: 'row' }}>
               <Character mood={playerThatAsks.character} />
               <Character mood={playerThatAnswers.character} flip />
+            </View>
+            <View style={styles.recordingContainer}>
+              {isRecording ?
+                  <View style={{ justifyContent: "space-between", flexDirection: 'row' }}>
+                    <TouchableOpacity onPress={() => {stopRecording()}}>
+                      <FontAwesome6 name="circle-stop" size={24} color={colors.orange[200]} />
+                    </TouchableOpacity>
+                    <Text style={styles.recordingText}>
+                      {formatTime(recorderState.durationMillis)}
+                    </Text>
+                    <View />
+                  </View>
+                :
+                  <View style={{ justifyContent: "space-between", flexDirection: 'row' }}>
+                    <TouchableOpacity onPress={() => {startRecording()}}>
+                      <FontAwesome6 name="microphone" size={24} color={colors.orange[200]} />
+                    </TouchableOpacity>
+                    <Text style={styles.recordingText}>{audioRecorder.uri ? "Record a new answer" : "Record answer"}</Text>
+                    <View />
+                  </View>
+              }
             </View>
           </View>
         </View>
@@ -129,6 +200,23 @@ const styles = StyleSheet.create({
   },
   playerThatAnswers: {
     color: colors.orange[200],
+  },
+  recordingContainer: {
+    marginTop: verticalScale(20),
+    // alignItems: 'center',
+    backgroundColor: colors.gray[300],
+    paddingVertical: scale(15),
+    paddingHorizontal: scale(15),
+    width: scale(250),
+    alignSelf: 'center',
+    borderRadius: moderateScale(10),
+  },
+  recordingText: {
+    fontSize: moderateScale(16),
+    color: colors.orange[200],
+    fontFamily: 'Ralway',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   question: {
     fontSize: moderateScale(18),
